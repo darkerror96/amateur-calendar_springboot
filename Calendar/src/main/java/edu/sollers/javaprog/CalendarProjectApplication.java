75777ba2d1d5d5c9d85be8918717c7f0de15872e
rutpatel <rutpatel@10.155.21.207>
2019-03-25 15:00:19 -0400

    Master Commit

---
+++ Calendar/src/main/java/edu/sollers/javaprog/CalendarProjectApplication.java
@@ -0,0 +1,18 @@
+package edu.sollers.javaprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author rutpatel
+ *
+ */
+
+@SpringBootApplication
+public class CalendarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarProjectApplication.class, args);
+	}
+}
---
+++ Calendar/src/test/java/edu/sollers/javaprog/CalendarProjectApplicationTests.java
@@ -0,0 +1,22 @@
+package edu.sollers.javaprog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 
+ * @author rutpatel
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
